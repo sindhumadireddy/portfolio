88ba0e29fc727be197a02366fcdde9fa4b083e1a
sindhumadireddy <sindhumadireddy@outlook.com>
2026-01-14 12:42:21 -0600

    updating vite.config

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <h1 style={{ padding: 20 }}>Portfolio is live ✅</h1>
   </StrictMode>,
 )
